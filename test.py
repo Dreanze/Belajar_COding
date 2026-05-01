bc730d08d65f010f088e0c07f91d82bd626300a9
Dreanze <sani.diaz12@gmail.com>
2026-05-01 21:28:07 +0700

    diedit dikit

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print("Hello World")
+
+a = 2
+b = 3
+c = 4
+
+pertambahan = a+b
+pengurangan = a-b
+perkalian = a*b
+pembangian = a/b
+
+print(f"Hasil pertambahan anda adalah : {pertambahan}")
+print(f'Hasil Pengurangan anda adalah : {pengurangan}')
